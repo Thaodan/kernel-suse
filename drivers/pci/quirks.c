b9df1dfb9e2af5606d0370f6f0fcb38b4a9c4fe4
Bogdan Lezhepekov <blezhepekov@suse.de>
2022-07-19 12:04:34 +0300

    pci: Split HSM & IFSI into separate IOMMU groups
    In order for HSM & IFSI to work in parallel
    split the two functions up into separate IOMMU groups.
    
    Requested by Elektrobit to ship as part of Carwos (jsc#EB-390).
    
    Signed-off-by: Bogdan Lezhepekov <blezhepekov@suse.de>
    
    suse-commit: 1c63dbacb0169f6d203dff973c2f589c4c3f0e75

--- drivers/pci/quirks.c
+++ drivers/pci/quirks.c
@@ -5014,6 +5014,8 @@ static const struct pci_dev_acs_enabled {
 	{ PCI_VENDOR_ID_NXP, 0x8d9b, pci_quirk_nxp_rp_acs },
 	/* Zhaoxin Root/Downstream Ports */
 	{ PCI_VENDOR_ID_ZHAOXIN, PCI_ANY_ID, pci_quirk_zhaoxin_pcie_ports_acs },
+	{ PCI_VENDOR_ID_INTEL, 0x19e5, pci_quirk_mf_endpoint_acs },
+	{ PCI_VENDOR_ID_INTEL, 0x19e6, pci_quirk_mf_endpoint_acs },
 	{ 0 }
 };
 
