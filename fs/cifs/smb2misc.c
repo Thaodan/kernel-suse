ba8a698b2db3531142a30d22977b046ce8758086
Enzo Matsumiya <ematsumiya@suse.de>
2022-08-26 20:18:46 -0300

    cifs: remove check of list iterator against head past the loop body (bsc#1190317).
    suse-commit: 53771a642740aa529149ee109b6aa238a19b773b

--- fs/cifs/smb2misc.c
+++ fs/cifs/smb2misc.c
@@ -163,15 +163,18 @@ smb2_check_message(char *buf, unsigned int len, struct TCP_Server_Info *srvr)
 		struct smb2_transform_hdr *thdr =
 			(struct smb2_transform_hdr *)buf;
 		struct cifs_ses *ses = NULL;
+		struct cifs_ses *iter;
 
 		/* decrypt frame now that it is completely read in */
 		spin_lock(&cifs_tcp_ses_lock);
-		list_for_each_entry(ses, &srvr->smb_ses_list, smb_ses_list) {
-			if (ses->Suid == thdr->SessionId)
+		list_for_each_entry(iter, &srvr->smb_ses_list, smb_ses_list) {
+			if (iter->Suid == thdr->SessionId) {
+				ses = iter;
 				break;
+			}
 		}
 		spin_unlock(&cifs_tcp_ses_lock);
-		if (&ses->smb_ses_list == &srvr->smb_ses_list) {
+		if (!ses) {
 			cifs_dbg(VFS, "no decryption - session id not found\n");
 			return 1;
 		}
