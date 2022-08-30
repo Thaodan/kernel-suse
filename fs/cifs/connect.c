adc48981e5e5c8513d2e7442103ace18077b3b72
Enzo Matsumiya <ematsumiya@suse.de>
2022-08-30 20:30:00 -0300

    cifs: remove repeated debug message on cifs_put_smb_ses() (bsc#1190317).
    suse-commit: 55e93f13e1f37efdda5c8b197457466333b26b92

--- fs/cifs/connect.c
+++ fs/cifs/connect.c
@@ -3193,7 +3193,6 @@ void cifs_put_smb_ses(struct cifs_ses *ses)
 {
 	unsigned int rc, xid;
 	struct TCP_Server_Info *server = ses->server;
-	cifs_dbg(FYI, "%s: ses_count=%d\n", __func__, ses->ses_count);
 
 	spin_lock(&cifs_tcp_ses_lock);
 	if (ses->status == CifsExiting) {
