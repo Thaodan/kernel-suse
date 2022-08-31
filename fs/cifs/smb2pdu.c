3b712a42b081bff1a53df2e11cdc54cc02177e20
Enzo Matsumiya <ematsumiya@suse.de>
2022-08-31 17:35:33 -0300

    cifs: move from strlcpy with unused retval to strscpy (bsc#1190317).
    suse-commit: bb4c21db895222b4ec203fe85b61f7e486af5729

--- fs/cifs/smb2pdu.c
+++ fs/cifs/smb2pdu.c
@@ -1737,7 +1737,7 @@ SMB2_tcon(const unsigned int xid, struct cifs_ses *ses, const char *tree,
 	tcon->tidStatus = CifsGood;
 	tcon->need_reconnect = false;
 	tcon->tid = rsp->sync_hdr.TreeId;
-	strlcpy(tcon->treeName, tree, sizeof(tcon->treeName));
+	strscpy(tcon->treeName, tree, sizeof(tcon->treeName));
 
 	if ((rsp->Capabilities & SMB2_SHARE_CAP_DFS) &&
 	    ((tcon->share_flags & SHI1005_FLAGS_DFS) == 0))
