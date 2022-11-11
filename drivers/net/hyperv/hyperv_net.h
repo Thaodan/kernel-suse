56d9688d55d382318a7ad853a42089b034480ab4
Olaf Hering <ohering@suse.de>
2022-11-11 11:11:11 +0000

    hv_netvsc: Add the support of hibernation (bsc#1204017).
    suse-commit: d29c11d0c02c387a1e77c7555a14ce09af4c89a4

--- drivers/net/hyperv/hyperv_net.h
+++ drivers/net/hyperv/hyperv_net.h
@@ -973,6 +973,7 @@ struct net_device_context {
 	/* Serial number of the VF to team with */
 	u32 vf_serial;
 
+	/* Used to temporarily save the config info across hibernation */
 	struct netvsc_device_info *saved_netvsc_dev_info;
 };
 
