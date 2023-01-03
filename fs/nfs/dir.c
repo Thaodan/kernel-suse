40ecbbe92dd79a51d33da74af9b52fb340c39e8a
NeilBrown <neilb@suse.de>
2023-01-03 11:25:19 +1100

    NFSv4: Fix return value in nfs_finish_open() (git-fixes).
    suse-commit: 834d49fde52278a4a0624f5002a3af0af9e76fba

--- fs/nfs/dir.c
+++ fs/nfs/dir.c
@@ -1522,8 +1522,10 @@ static int nfs_finish_open(struct nfs_open_context *ctx,
 	err = finish_open(file, dentry, do_open, opened);
 	if (err)
 		goto out;
-	nfs_file_set_open_context(file, ctx);
-
+	if (S_ISREG(file->f_path.dentry->d_inode->i_mode))
+		nfs_file_set_open_context(file, ctx);
+	else
+		err = -EOPENSTALE;
 out:
 	return err;
 }
